755bb2ea4e37b6647c499def599d130f577fe780
Jeff Tian <jefftian@hcdlearning.com>
2017-08-07 17:29:36 +0800

    add daemon

---
+++ hongda/daemon_app.py
@@ -0,0 +1,26 @@
+from bottle import route, run, get, post, request
+import urllib
+import json
+from api.subtitle import burn
+from bottledaemon import daemon_run
+
+
+@route('/hello')
+def hello():
+    return "Hello World!"
+
+
+@route('/healthcheck')
+def healthcheck():
+    return "OK"
+
+
+@post('/burn_subtitle')
+def burn_subtitle():
+    burn(srtPath=request.json['srtPath'],
+         videoPath=request.json['videoPath'], outputPath=request.json['outputPath'])
+    return 'done'
+
+
+if __name__ == '__main__':
+    daemon_run(host='0.0.0.0', port=10126)
