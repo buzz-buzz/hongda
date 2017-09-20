81a396cc8cd1d0178d39cb9970365cbd495f11ca
Jeff Tian <jefftian@hcdlearning.com>
2017-09-20 12:25:55 +0800

    Add sun glasses effect test

--- hongda/api/human_face_detector.py
+++ hongda/api/human_face_detector.py
@@ -7,96 +7,25 @@ import numpy as np
 import os.path
 import os
 import dlib
+from api.image_helper import *
+
+__all__ = ['paste_face', 'recipe_nose', 'recipe_sun_glasses']
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 nose_img_file = os.path.join(os.getcwd(), 'hongda/pasters/nose.jpg')
 nose_paster = cv2.imread(nose_img_file)
+
+sun_glasses_file = os.path.realpath(os.path.join(dir_path, '../pasters/sunglasses.jpg'))
+sun_glasses_paster = cv2.imread(sun_glasses_file)
 
 face_detector = dlib.get_frontal_face_detector()
 landmark_file_path = os.path.join(os.getcwd(), 'hongda/data/shape_predictor_68_face_landmarks.dat')
 predictor = dlib.shape_predictor(landmark_file_path)
 
-def shape_to_np(shape, dtype="int"):
-    coords = np.zeros((68, 2), dtype=dtype)
 
-    for i in range(0, 68):
-        coords[i] = (shape.part(i).x, shape.part(i).y)
-
-    return coords
-
-
-def rect_to_bounding_box(rect):
-    x = rect.left()
-    y = rect.top()
-    w = rect.right() - x
-    h = rect.bottom() - y
-
-    return (x, y, w, h)
-
-
-def sub_image(img, x, y, w, h):
-    clip = sub_area(img, x, y, w, h)
-    return img[clip[1]:clip[1] + clip[3], clip[0]:clip[0] + clip[2]]
-
-def sub_area(img, x, y, w, h):
-    print('image shape = ', img.shape)
-    print(x, y, w, h)
-    if x < 0:
-        crop_x = -x
-        x = 0
-        w -= crop_x
-    else:
-        crop_x = 0
-
-    if y < 0:
-        print('y is less than zero!')
-        crop_y = -y
-        y = 0
-        h -= crop_y
-    else:
-        crop_y = 0
-
-    if w > img.shape[1] - x:
-        w = img.shape[1] - x
-
-    if h > img.shape[0] - y:
-        print('h is overflow!')
-        h = img.shape[0] - y
-
-    crop_end_x = crop_x + w
-    crop_end_y = crop_y + h
-    print(x, y, w, h)
-    return (x, y, w, h, crop_x, crop_y, crop_end_x, crop_end_y)
-
-
-def paster_nose_to(img, paster, center_x, center_y, paste_to_width, paste_to_height):
-    if paste_to_height == None:
-        paste_to_height = int((paster.shape[0] / paster.shape[1]) * paste_to_width)
-    paster = cv2.resize(paster, (paste_to_width, paste_to_height), interpolation=cv2.INTER_AREA)
-    start_x = int(center_x - paste_to_width / 2)
-    start_y = int(center_y - paste_to_height / 2)
-
-    clip = sub_area(img, start_x, start_y, paste_to_width, paste_to_height)
-
-    _, paster_mask = cv2.threshold(
-        cv2.cvtColor(paster, cv2.COLOR_BGR2GRAY), 200, 255, cv2.THRESH_BINARY_INV)
-    paster = paster[clip[5]:clip[7], clip[4]:clip[6]]
-    paster_mask = paster_mask[clip[5]:clip[7], clip[4]:clip[6]]
-    inv_paster_mask = cv2.bitwise_not(paster_mask)
-    masked_paster = cv2.bitwise_and(paster, paster, mask=paster_mask)
-
-    nose_area = sub_image(img, start_x, start_y, paste_to_width, paste_to_height)
-    masked_nose_area = cv2.bitwise_and(nose_area, nose_area, mask=inv_paster_mask)
-    merged = cv2.add(masked_nose_area, masked_paster)
-
-    img[clip[1]:clip[1] + clip[3],
-    clip[0]:clip[0] + clip[2]] = merged
-
-    return img
-
-
-def paster_nose_2(img):
+def paste_face(img, pasters):
     scale = 200 / min(img.shape[1], img.shape[0])
-    print('scale= ', scale)
     gray = cv2.resize(img, None, fx=scale, fy=scale, interpolation=cv2.INTER_AREA)
     gray = cv2.cvtColor(gray, cv2.COLOR_BGR2GRAY)
 
@@ -106,7 +35,12 @@ def paster_nose_2(img):
         shape = predictor(gray, face_rect)
         shape = shape_to_np(shape)
         (x, y, w, h) = rect_to_bounding_box(face_rect)
-        cv2.circle(img, (int(shape[30][0] / scale), int(shape[30][1] / scale)), 2, (255, 0, 0), -1)
-        paster_nose_to(img, nose_paster, int(shape[30][0] / scale), int(shape[30][1] / scale), int((w / scale) * 1.25), None)
+        for paster, landmark_index, ratio in pasters:
+            add_paster(img, paster, int(shape[landmark_index][0] / scale), int(shape[landmark_index][1] / scale),
+                       int(ratio * w / scale), None)
 
     return img
+
+
+recipe_nose = (nose_paster, 30, 1.25)
+recipe_sun_glasses = (sun_glasses_paster, 27, 1)
--- hongda/tests_human_face_detector.py
+++ hongda/tests_human_face_detector.py
@@ -1,21 +1,34 @@
 import unittest
 import os
-# from api.human_face_detector import *
+from api.human_face_detector import *
 import cv2
+from skimage.measure import compare_mse as im_diff
+
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 class HumanFaceDetectorTest(unittest.TestCase):
 
-    def test_shap_to_np(self):
-
-    
-    def test_human_face_detect(self):
+    def test_add_nose(self):
         img_path = os.path.join(dir_path, 'tests/xinwen.jpg')
+        nosed_img_path = os.path.join(dir_path, 'tests/n-xinwen.jpg')
         img = cv2.imread(img_path)
-        cv2.imshow( 'output', img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        paste_face(img, [recipe_nose])
+        expected_nosed_img = cv2.imread(nosed_img_path)
+        diff = im_diff(img, expected_nosed_img)
+        print('diff = ', diff)
+        self.assertLess(diff, 1)
+
+    def test_add_sun_glasses(self):
+        img_path = os.path.join(dir_path, 'tests/xinwen.jpg')
+        sun_glasses_img_path = os.path.join(dir_path, 'tests/e-xinwen.jpg')
+        img = cv2.imread(img_path)
+        paste_face(img, [recipe_sun_glasses])
+        expected = cv2.imread(sun_glasses_img_path)
+
+        diff = im_diff(img, expected)
+        print('diff == ', diff)
+        self.assertLess(diff, 1)
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
